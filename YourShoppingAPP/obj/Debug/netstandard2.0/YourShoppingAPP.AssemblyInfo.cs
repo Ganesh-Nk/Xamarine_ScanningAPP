ffd7f721a290cf5f21d053f97aeea2de4beaff3a
Ganesh Navada K <gnakx@eg.dk>
2025-03-13 17:38:06 +0530

    UI design

--- YourShoppingAPP/obj/Debug/netstandard2.0/YourShoppingAPP.AssemblyInfo.cs
+++ YourShoppingAPP/obj/Debug/netstandard2.0/YourShoppingAPP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourShoppingAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5161035e5b122b975641f57d6ca186f6d2912706")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c7c2bd8258f080f9c5f8d43b208f15af72c9a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourShoppingAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourShoppingAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
